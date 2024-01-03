161a8759b1e11f5d0b5f2f9c45a35c830b75b898
malantivora04 <146733377+malantivora04@users.noreply.github.com>
2024-01-03 19:16:24 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# counter-increase-decrease
+ counter with increase decrease button which change the value dynamically using javascript
---
+++ counter.js
@@ -0,0 +1,12 @@
+var num = document.getElementById("p1");
+num = 0;
+
+function inc(){
+    document.getElementById('p1').innerHTML= num = num+1;
+}
+function dec(){
+    document.getElementById('p1').innerHTML= num = num - 1;
+}
+function add2(){
+    document.getElementById('p1').innerHTML= num = num+2;
+}
